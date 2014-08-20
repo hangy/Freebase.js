4fe01ca6570b6ba287bcdaae587616013a621ded
hangy <hangy@hangy.de>
2014-08-20 03:00:57 +0200

    Added stupid simple write sample. Closes #1.

---
+++ client_side/lib/fbconfig.js
@@ -0,0 +1,3 @@
+var API_KEY = 'dcbQTFzE52zmU2KJnSNPgpn6JhnWyCWWXb3sZhw';
+var CLIENT_ID = '1665340704534-9PJaWnA8Ud7h29YtCLu3e6kGeNqkDPjr.apps.googleusercontent.com';
+var CLIENT_SECRET = 'Mfyvev6JsVzuaVgmBb4EQmRn';
